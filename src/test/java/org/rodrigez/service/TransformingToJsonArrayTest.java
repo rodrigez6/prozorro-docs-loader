c3acdc10dfb69bc98b5acc7691eee9791ac832fd
mykola <rodrigez6@ukr.net>
2019-01-23 23:50:19 +0200

    -- tests

--- src/test/java/org/rodrigez/service/TransformingToJsonArrayTest.java
+++ src/test/java/org/rodrigez/service/TransformingToJsonArrayTest.java
@@ -1,18 +1,13 @@
 package org.rodrigez.service;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.print.Doc;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
